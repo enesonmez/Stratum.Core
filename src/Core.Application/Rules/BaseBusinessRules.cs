7afd67dae67c811cc09d8553c0813541e2977145
Enes Sönmez <sonmezens@hotmail.com>
2025-07-06 21:07:35 +0300

    feat(Core.Security): added OperationClaim files

--- src/Core.Application/Rules/BaseBusinessRules.cs
+++ src/Core.Application/Rules/BaseBusinessRules.cs
@@ -1,6 +1,20 @@
+using Core.CrossCuttingConcerns.Exception.Types;
+using Core.Localization.Abstraction;
+
 namespace Core.Application.Rules;
 
 public abstract class BaseBusinessRules
 {
+    private readonly ILocalizationService _localizationService;
+
+    protected BaseBusinessRules(ILocalizationService localizationService)
+    {
+        _localizationService = localizationService;
+    }
     
+    protected async Task ThrowBusinessException(string messageKey, string sectionName)
+    {
+        string message = await _localizationService.GetLocalizedAsync(messageKey, sectionName);
+        throw new BusinessException(message);
+    }
 }
